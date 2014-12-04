980a632f9834be98cce4c289b2d895e2a91e2c6d
Xiaowen Zhang <xiaowen.z@icloud.com>
2014-12-04 20:42:33 +0800

    First commit

---
+++ MacOSSwiftHelloWorld/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MacOSSwiftHelloWorld
+//
+//  Created by Xiaowen Zhang on 12/4/14.
+//  Copyright (c) 2014 Xiaowen Zhang. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
