f89b445fa923b4c3caaa109b0a00096553262b98
Xiaowen Zhang <xiaowen.z@icloud.com>
2014-12-04 23:28:34 +0800

    Hello world function works.

--- MacOSSwiftHelloWorld/ViewController.swift
+++ MacOSSwiftHelloWorld/ViewController.swift
@@ -10,10 +10,29 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+  
+    @IBOutlet weak var textField: NSTextField!
+    
+    
+    @IBAction func onClickAction(sender: AnyObject) {
+        var helloString:String = "Hello World!"
+        if (self.textField.stringValue != helloString) {
+            self.textField.stringValue = "Hello World!"
+        }else{
+            self.textField.stringValue = "";
+        }
+        
+        //println("test")
+        
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        
+        self.textField.stringValue = "Hello You need to Push the button"
     }
 
     override var representedObject: AnyObject? {
